fdb7cee7c88d0514ccd61f2f4d8ed0f70cb0ffa4
Thibaut Dhalluin <thibaut.dhallu@gmail.com>
2018-02-20 13:30:34 +0100

    PREMIERCOMMENTAIREgitCOMMIT

---
+++ casse/changeCasse.cpp
@@ -0,0 +1,19 @@
+// changeCasse.cpp
+// g++ -std=c++11 -Wall -Wextra -o changeCasse.out changeCasse.cpp
+#include <cctype>
+#include <iostream>
+int main()
+{
+    std::string texte = "Toto, Tata";
+    // change les minuscules par des majuscules et réciproquement
+    for (char & c : texte)
+    {
+        if (std::islower(c))
+            std::cout << char(std::toupper(c));
+        else if (std::isupper(c))
+            std::cout << char(std::tolower(c));
+        else 
+            std::cout << c;
+    }
+    return 0;
+}
