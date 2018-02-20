36e2102c1f3b4fa25736659a64ffa6d2fa0da84b
Thibaut Dhalluin <thibaut.dhallu@gmail.com>
2018-02-20 14:42:50 +0100

    AJOUT_REAMDE

--- casse/changeCasse.cpp
+++ casse/changeCasse.cpp
@@ -2,18 +2,29 @@
 // g++ -std=c++11 -Wall -Wextra -o changeCasse.out changeCasse.cpp
 #include <cctype>
 #include <iostream>
+
 int main()
 {
-    std::string texte = "Toto, Tata";
-    // change les minuscules par des majuscules et réciproquement
-    for (char & c : texte)
+  std::string texte;
+  std::string newtexte = " ";
+  std::cin >> texte;  
+  while (std::cin >> newtexte)
     {
-        if (std::islower(c))
-            std::cout << char(std::toupper(c));
-        else if (std::isupper(c))
-            std::cout << char(std::tolower(c));
-        else 
-            std::cout << c;
+      texte += newtexte;
     }
-    return 0;
+
+  std::cout << texte << std::endl;
+  
+  // change les minuscules par des majuscules et réciproquement
+  for (char & c : texte)
+    {
+      if (std::islower(c))
+	std::cout << char(std::toupper(c));
+      else if (std::isupper(c))
+	std::cout << char(std::tolower(c));
+      else 
+	std::cout << c;
+    }
+  std::cout << std::endl;
+  return 0;
 }
